a3b6becd68dccc21b3abeb22e702aa33dc15688c
Andre Wagner <WagnerAndre@JohnDeere.com>
2025-03-05 14:47:24 -0600

    .

--- pl_log.h
+++ pl_log.h
@@ -1,29 +1,42 @@
 #ifndef PL_LOG_H
 #define PL_LOG_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdbool.h>
+#include <stdio.h>
+
 typedef enum pl_Level { PL_TRACE_LEVEL = 2, PL_DEBUG_LEVEL = 1, PL_INFO_AND_UP = 0 } pl_Level;
 
-void        pl_init();
-void        pl_close();
-void        pl_set_level(pl_Level level);
-const char* pl_last_error();
-void        pl_set_warning_callback(void (*cb)(void* data), void* data);
-void        pl_set_error_callback(void (*cb)(void* data), void* data);
-void        pl_set_abort_callback(void (*cb)(void* data), void* data);
+extern void        pl_init();
+extern void        pl_close();
+extern pl_Level    pl_level();
+extern void        pl_set_level(pl_Level level);
+extern const char* pl_last_error();
+extern void        pl_set_warning_callback(void (*cb)(void* data), void* data);
+extern void        pl_set_error_callback(void (*cb)(void* data), void* data);
+extern void        pl_set_abort_callback(void (*cb)(void* data), void* data);
+
+extern void pl_log(pl_Level level, const char* color, const char* lvl, bool copy_error, const char* msg, ...) __attribute__((format(printf, 5, 6)));
+
+extern void call_warning_callback();
+extern void call_error_callback();
+extern void call_abort_callback();
 
 #define PL_TRACE(msg, ...) { pl_log(PL_TRACE_LEVEL, "\e[90m", "TRACE", false, msg, ##__VA_ARGS__); }
 #define PL_DEBUG(msg, ...) { pl_log(PL_DEBUG_LEVEL, "\e[33m", "DEBUG", false, msg, ##__VA_ARGS__); }
 #define PL_INFO(msg, ...) { pl_log(PL_INFO_AND_UP, "\e[0m", "INFO ", false, msg, ##__VA_ARGS__); }
-#define PL_WARNING(msg, ...) { pl_log(PL_INFO_AND_UP, "\e[95m", "WARN ", false, msg, ##__VA_ARGS__); if (pl_warning_callback_) pl_warning_callback_(pl_warning_callback_data_); }
-#define PL_ERROR(msg, ...) { pl_log(PL_INFO_AND_UP, "\e[91m", "ERROR", true, msg, ##__VA_ARGS__); if (pl_error_callback_) pl_error_callback_(pl_error_callback_data_); return; }
-#define PL_ERROR_RET(code, msg, ...) { pl_log(PL_INFO_AND_UP, "\e[91m", "ERROR", true, msg, ##__VA_ARGS__); if (pl_error_callback_) pl_error_callback_(pl_error_callback_data_); return code; }
-#define PL_ABORT() { pl_log(PL_INFO_AND_UP, "\e[31m", "CRITICAL", true, "Aborting due to programming error"); if (pl_abort_callback_) pl_abort_callback_(pl_abort_callback_data_); abort(); }
+#define PL_WARNING(msg, ...) { pl_log(PL_INFO_AND_UP, "\e[95m", "WARN ", false, msg, ##__VA_ARGS__); call_warning_callbacl(); }
+#define PL_ERROR(msg, ...) { pl_log(PL_INFO_AND_UP, "\e[91m", "ERROR", true, msg, ##__VA_ARGS__); call_error_callback(); return; }
+#define PL_ERROR_RET(code, msg, ...) { pl_log(PL_INFO_AND_UP, "\e[91m", "ERROR", true, msg, ##__VA_ARGS__); call_error_callback(); return code; }
+#define PL_ABORT() { pl_log(PL_INFO_AND_UP, "\e[31m", "CRITICAL", true, "Aborting due to programming error"); call_abort_callback(); abort(); }
 
 #ifdef PL_LOG_IMPLEMENTATION
 
 #include <errno.h>
 #include <stdarg.h>
-#include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -34,7 +47,7 @@ void        pl_set_abort_callback(void (*cb)(void* data), void* data);
 pl_Level    pl_level_ = PL_INFO_AND_UP;
 bool        pl_color_ = true;
 bool        pl_levelname_ = true;
-const char  pl_last_error_[PL_ERROR_SZ] = "";
+char        pl_last_error_[PL_ERROR_SZ] = "";
 void        (*pl_warning_callback_)(void* data);
 void*       pl_warning_callback_data_;
 void        (*pl_error_callback_)(void* data);
@@ -81,6 +94,11 @@ void pl_close()
         fclose(pl_file_);
 }
 
+pl_Level pl_level()
+{
+    return pl_level_;
+}
+
 void pl_set_level(pl_Level level)
 {
     pl_level_ = level;
@@ -111,7 +129,10 @@ void pl_set_abort_callback(void (*cb)(void* data), void* data)
 
 void pl_log(pl_Level level, const char* color, const char* lvl, bool copy_error, const char* msg, ...)
 {
-    if (level < pl_level_)
+    if (!pl_file_)
+        pl_file_ = stderr;
+
+    if (level > pl_level_)
         return;
     if (pl_color_)
         fprintf(pl_file_, "%s", color);
@@ -130,6 +151,28 @@ void pl_log(pl_Level level, const char* color, const char* lvl, bool copy_error,
     fprintf(pl_file_, "\n");
 }
 
+void call_warning_callback()
+{
+    if (pl_warning_callback_)
+        pl_warning_callback_(pl_warning_callback_data_);
+}
+
+void call_error_callback()
+{
+    if (pl_error_callback_)
+        pl_error_callback_(pl_error_callback_data_);
+}
+
+void call_abort_callback()
+{
+    if (pl_abort_callback_)
+        pl_abort_callback_(pl_abort_callback_data_);
+}
+
+#endif
+
+#ifdef __cplusplus
+}
 #endif
 
 #endif //PL_LOG_H
